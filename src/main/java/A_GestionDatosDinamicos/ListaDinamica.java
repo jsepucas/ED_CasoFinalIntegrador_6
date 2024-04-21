c6dd0d911287a320b0e14af3582dabae97f558f3
jsepucas <jsepucas@myuax.com>
2024-04-21 20:38:34 +0200

    Commits

---
+++ src/main/java/A_GestionDatosDinamicos/ListaDinamica.java
@@ -0,0 +1,11 @@
+package A_GestionDatosDinamicos;
+
+import java.util.ArrayList;
+public class ListaDinamica <T>  {
+
+    private ArrayList<T> elementos;
+
+    public ListaDinamica() {
+        elementos = new ArrayList<>();
+    }
+}
