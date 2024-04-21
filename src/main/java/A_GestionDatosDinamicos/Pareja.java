af88a9dc6f11b95a9e1e8c48999fd00d7a25707e
jsepucas <jsepucas@myuax.com>
2024-04-21 20:36:48 +0200

    Commits

--- src/main/java/A_GestionDatosDinamicos/Pareja.java
+++ src/main/java/A_GestionDatosDinamicos/Pareja.java
@@ -6,5 +6,9 @@ public class Pareja {
     private int primerElemento;
     private int segundoElemento;
 
+    public Pareja(int primerElemento, int segundoElemento) {
+        this.primerElemento = primerElemento;
+        this.segundoElemento = segundoElemento;
+    }
 
 }
