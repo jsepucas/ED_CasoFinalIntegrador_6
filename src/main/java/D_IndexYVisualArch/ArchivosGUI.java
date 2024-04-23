c4a575d6b9b4a6b649bbf665f829326de82ca03a
jsepucas <jsepucas@myuax.com>
2024-04-23 10:59:08 +0200

    Commitss

--- src/main/java/D_IndexYVisualArch/ArchivosGUI.java
+++ src/main/java/D_IndexYVisualArch/ArchivosGUI.java
@@ -8,3 +8,13 @@ public class ArchivosGUI extends JFrame {
     private JTextField inputRuta;
     private DefaultListModel<String> modeloLista;
     private JList<String> listaArchivos;
+
+    public ArchivosGUI() {
+        super("Indexación y Visualización de Archivos");
+        indexador = new IndexadorArchivos();
+        initComponents();
+        setSize(600, 400);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
