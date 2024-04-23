3ec471b176ab27700099f2cd7f7e83f5dc1b6b9e
jsepucas <jsepucas@myuax.com>
2024-04-23 14:29:02 +0200

    Commitsss MainGUI

--- src/main/java/B_AnalisisOrg_Info/AnalisisOrgInfoGUI.java
+++ src/main/java/B_AnalisisOrg_Info/AnalisisOrgInfoGUI.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+
 public class AnalisisOrgInfoGUI extends JFrame {
     private Ordenador ordenador;
     private GestorVentas gestorVentas;
