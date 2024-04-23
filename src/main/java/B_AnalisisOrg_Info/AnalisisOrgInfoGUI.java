ec639c9cf407351e3faebd1706b9cd09c0ae21ff
jsepucas <jsepucas@myuax.com>
2024-04-23 09:21:09 +0200

    Commitss

---
+++ src/main/java/B_AnalisisOrg_Info/AnalisisOrgInfoGUI.java
@@ -0,0 +1,10 @@
+package B_AnalisisOrg_Info;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class AnalisisOrgInfoGUI {
+}
