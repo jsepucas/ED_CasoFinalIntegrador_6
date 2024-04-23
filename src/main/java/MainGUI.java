6e70a88b1ae3f1c1f8255e1fae1764e664975343
jsepucas <jsepucas@myuax.com>
2024-04-23 13:50:42 +0200

    Commitsss MainGUI

--- src/main/java/MainGUI.java
+++ src/main/java/MainGUI.java
@@ -6,3 +6,12 @@ import B_AnalisisOrg_Info.AnalisisOrgInfoGUI;
 import C_MapasYDatos.MapasYDatosGUI;
 import D_IndexYVisualArch.ArchivosGUI;
 
+public class MainGUI extends JFrame {
+    public MainGUI() {
+        super("Aplicación Principal - Sistema de Gestión");
+        initComponents();
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
