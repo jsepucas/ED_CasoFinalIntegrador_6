5b03b7361e0690c8d1ec4b5ab24df8cc72b61d80
jsepucas <jsepucas@myuax.com>
2024-04-23 09:11:43 +0200

    Commitss

--- src/main/java/B_AnalisisOrg_Info/Ordenador.java
+++ src/main/java/B_AnalisisOrg_Info/Ordenador.java
@@ -1,9 +1,13 @@
 package B_AnalisisOrg_Info;
 
-
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Ordenador {
     public static void ordenarNombres(ArrayList<String> lista) {
         Collections.sort(lista);
+    }
+    public static int buscarNombre(ArrayList<String> lista, String nombre) {
+        return Collections.binarySearch(lista, nombre);
     }
 }
