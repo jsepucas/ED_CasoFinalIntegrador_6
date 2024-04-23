f46f2f4f807ad998272329b5b27247699c1e1e9a
jsepucas <jsepucas@myuax.com>
2024-04-23 16:00:57 +0200

    Commits diseño

--- src/main/java/MainGUI.java
+++ src/main/java/MainGUI.java
@@ -7,13 +7,14 @@ import A_GestionDatosDinámicos.InterfazGrafica;
 import B_AnalisisOrg_Info.AnalisisOrgInfoGUI;
 import C_MapasYDatos.MapasYDatosGUI;
 import D_IndexYVisualArch.ArchivosGUI;
+import E_Fondo.FondoPanel;
 
 // -------------------------Clase MainGUI-------------------------
 public class MainGUI extends JFrame {
 
     public MainGUI() {
 
-        super("Sistema de Gestión y Análisis de Datos Multidimensionales de la UAX \uD83D\uDCCA ");
+        super("Sistema de Gestión y Análisis de Datos Multidimensionales de la UAX  \uD83D\uDCCA ");
         initComponents();
         setSize(1020, 710);
         setLocationRelativeTo(null);
@@ -25,13 +26,22 @@ public class MainGUI extends JFrame {
 //-------------------------Código de la aplicación principal-------------------------
 
     private void initComponents() {
+        //---------------------------------------------------------------//  Para editar el fondo???
 
-        //Layout para organizar los botones de manera uniforme.
+        FondoPanel fondoPanel = new FondoPanel("src/main/resources/Logo.png");
+        fondoPanel.setLayout(new BorderLayout());
 
-        setLayout(new GridLayout(2, 5, 10, 10)); // GridLayout para organizar los botones de manera uniforme.
+        JLabel imagenEmpresa = new JLabel(new ImageIcon("src/main/resources/Logo.png"));
+        fondoPanel.add(imagenEmpresa, BorderLayout.NORTH);
 
+
+        //Layout para organizar los botones de manera uniforme?
 
 //-------------------------------------------------------------------------------------------------------------------------------
+
+        //Declaración de los gbc's
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
 
         //Creación de los botones para cada módulo de la aplicación. (A, B, C, D)
         JButton btnModuloA = new JButton("Gestión de Datos Dinámicos");
@@ -45,15 +55,34 @@ public class MainGUI extends JFrame {
         btnModuloC.addActionListener(e -> new MapasYDatosGUI().setVisible(true));
         btnModuloD.addActionListener(e -> new ArchivosGUI().setVisible(true));
 
-        //Añadir los botones al contenedor principal.
 
-        add(btnModuloA);
-        add(btnModuloB);
-        add(btnModuloC);
-        add(btnModuloD);
+
+        //----------------------Configuración de los gbc's para cada botón.---------------------------
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.insets = new Insets(0, 0, 15, 15);
+        add(btnModuloA, gbc);
+
+        gbc.gridx = 3;
+        add(btnModuloB, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.gridwidth = 1; // Restablece gridwidth a 1 para los siguientes botones
+        add(btnModuloC, gbc);
+
+        gbc.gridx = 3;
+        add(btnModuloD, gbc);
+
+
+        //-------------------------Añadir el panel de fondo al contenedor principal.-------------------------
+
+        add(fondoPanel);
     }
 
     //-------------------------Método principal-------------------------
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(MainGUI::new);
     }
