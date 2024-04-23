ba6e8786aa3bf065c749ff61b81b913da83b3f4c
jsepucas <jsepucas@myuax.com>
2024-04-23 09:46:17 +0200

    Commitss

---
+++ src/main/java/C_MapasYDatos/MapasYDatosGUI.java
@@ -0,0 +1,9 @@
+package C_MapasYDatos;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class MapasYDatosGUI extends JFrame {
+    private GestorMapas gestorMapas;
+    private JTextField inputNumero, inputLetra, inputTexto;
+    private JTextArea areaResultados;
