3d2d4a1fe174fb20b0d11a6a55bfd9005ecbefc6
jsepucas <jsepucas@myuax.com>
2024-04-23 10:58:57 +0200

    Commitss

--- src/main/java/D_IndexYVisualArch/ArchivosGUI.java
+++ src/main/java/D_IndexYVisualArch/ArchivosGUI.java
@@ -2,3 +2,9 @@ package D_IndexYVisualArch;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+
+public class ArchivosGUI extends JFrame {
+    private IndexadorArchivos indexador;
+    private JTextField inputRuta;
+    private DefaultListModel<String> modeloLista;
+    private JList<String> listaArchivos;
