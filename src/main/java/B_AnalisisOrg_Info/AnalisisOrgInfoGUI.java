7cb76afc4c23238911157a54cb5a1a8f12d92320
jsepucas <jsepucas@myuax.com>
2024-04-23 09:21:46 +0200

    Commitss

--- src/main/java/B_AnalisisOrg_Info/AnalisisOrgInfoGUI.java
+++ src/main/java/B_AnalisisOrg_Info/AnalisisOrgInfoGUI.java
@@ -12,3 +12,14 @@ public class AnalisisOrgInfoGUI extends JFrame {
     private JTextField inputNombre, inputCliente, inputMonto, inputFecha;
     private DefaultListModel<String> listaNombresModel, listaVentasModel;
     private JList<String> listaNombres, listaVentas;
+
+    public AnalisisOrgInfoGUI() {
+        super("Análisis y Organización de Información");
+        ordenador = new Ordenador();
+        gestorVentas = new GestorVentas();
+        initComponents();
+        setSize(600, 400);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
