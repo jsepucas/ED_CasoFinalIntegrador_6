213dc7abb9651831431954a15035cd8cfc5474bd
jsepucas <jsepucas@myuax.com>
2024-04-23 09:37:32 +0200

    Commitss

--- src/main/java/C_MapasYDatos/GestorMapas.java
+++ src/main/java/C_MapasYDatos/GestorMapas.java
@@ -2,4 +2,13 @@ package C_MapasYDatos;
 import java.util.HashMap;
 import java.util.Map;
 
+public class GestorMapas {
+    private Map<Integer, Character> numerosLetras;
+    private Map<Integer, String> numerosTextos;
+
+    public GestorMapas() {
+        numerosLetras = new HashMap<>();
+        numerosTextos = new HashMap<>();
+        inicializarDatos();
+    }
 
