256f14145468863345e1ff75abcfc92d55a9e8b4
jsepucas <jsepucas@myuax.com>
2024-04-23 16:00:46 +0200

    Commits diseño

--- src/main/java/E_Fondo/FondoPanel.java
+++ src/main/java/E_Fondo/FondoPanel.java
@@ -8,7 +8,25 @@ public class FondoPanel extends JPanel {
 
     public FondoPanel(String rutaImagen) {
         imagen = new ImageIcon(rutaImagen).getImage();
+        initComponents();
     }
+
+    private void initComponents() {
+        setLayout(new BorderLayout());
+
+        JLabel imagenEmpresa = new JLabel(new ImageIcon(imagen));
+        imagenEmpresa.setHorizontalAlignment(JLabel.LEFT);
+        imagenEmpresa.setVerticalAlignment(JLabel.TOP);
+        add(imagenEmpresa, BorderLayout.NORTH);
+
+        JLabel sgadmLabel = new JLabel("S.G.A.D.M");
+        JLabel sgadmMeaningLabel = new JLabel("Sistema de Gestión y Análisis de Datos Multidimensionales");
+        JPanel sgadmPanel = new JPanel(new GridLayout(2, 1));
+        sgadmPanel.add(sgadmLabel);
+        sgadmPanel.add(sgadmMeaningLabel);
+        add(sgadmPanel, BorderLayout.NORTH);
+    }
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
